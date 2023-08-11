0ddc4d85c93bd44ff358077917191cb0d285158d
suntrackspb <spbwar@gmail.com>
2023-08-11 12:52:40 +0300

    add favorites, refactor

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import os
 import sys
 from pathlib import Path
+from typing import List
+from urllib.parse import unquote
+
+### CONSTANTS
+home_path: Path = Path.home()
+config_path: Path = Path.joinpath(home_path, ".config")
+
+dirs_file_path: str = str(Path.joinpath(config_path, "user-dirs.dirs"))
+locale_file_path: str = str(Path.joinpath(config_path, "user-dirs.locale"))
+bookmarks_path: str = str(Path.joinpath(config_path, "gtk-3.0", "bookmarks"))
+
+
+def read_from_file(filename: str) -> List[str]:
+    with open(filename, 'r') as file:
+        array = file.readlines()
+        return [x for x in array if x != "\n"]
+
+
+def write_to_file(filename: str, data: str, perm: str='a') -> None:
+    with open(filename, perm) as file:
+        file.write(data)
+
+
+BOOKMARKS: List[str] = [unquote(x) for x in read_from_file(bookmarks_path)]
+FOLDERS: List[str] = [x for x in read_from_file(dirs_file_path)]
+NEW_BOOKMARKS: List[str] = []
+
+os.rename(bookmarks_path, bookmarks_path + ".bak")
+os.rename(dirs_file_path, dirs_file_path + ".bak")
+
 
 
 def rename_folders():
-    home_path = Path.home()
-    dirs_file_path = home_path / ".config" / "user-dirs.dirs"
-    dirs_file_back = home_path / ".config" / "user-dirs.back"
-    locale_file_path = home_path / ".config" / "user-dirs.locale"
-
-    with open(dirs_file_path, "r") as dirs_file:
-        lines = dirs_file.readlines()
-    os.rename(dirs_file_path, dirs_file_back)
-    for line in lines:
+    for line in FOLDERS:
         if not line.startswith("#"):
             params, value = line.split("=")
             rus_name = value.split("/")[1].replace('"', '').rstrip()
@@ -24,14 +46,22 @@ def rename_folders():
                 os.rename(home_path / rus_name, home_path / eng_name)
             else:
                 os.mkdir(home_path / eng_name)
-            with open(dirs_file_path, "a") as dirs_file:
-                dirs_file.write(new_line + "\n")
+            fix_bookmarks(rus_name, eng_name)
+            write_to_file(dirs_file_path, new_line + "\n", "a")
 
-    with open(locale_file_path, "w") as locale_file:
-        locale_file.write("en_US\n")
+
+def fix_bookmarks(rus, eng):
+    for line in BOOKMARKS:
+        if line.split("/")[-1].replace("\n", "") == rus: 
+            new_line = line.replace(rus, eng)
+            NEW_BOOKMARKS.append(new_line)
+            
 
 
 if __name__ == '__main__':
     if sys.platform != 'linux':
         raise SystemExit("Incorrect system platform. You need Linux.")
     rename_folders()
+
+    write_to_file(locale_file_path, "en_US", "w")
+    write_to_file(bookmarks_path, "".join(NEW_BOOKMARKS), "w")
